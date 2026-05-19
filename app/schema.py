2cf7d6359e595138aeb618cd0b96297651209211
Md Tasrif Khan <tasrif.k.56@gmail.com>
2026-05-19 23:21:01 +0600

    Token Data schema class added

--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import date
-
+from typing import Optional
 
 #define request body schema
 class Garage(BaseModel):
@@ -29,3 +29,10 @@ class UsersCreate(BaseModel):
 class Login(BaseModel):
     email: EmailStr
     password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id : int | None = None
